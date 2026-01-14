09dfe1a84aaeae69141a0f88d1959f754a0c0e3f
alvedrake <alve.nielsen@elev.ga.lbs.se>
2026-01-14 11:42:18 +0100

    first commit

---
+++ text strory/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace text_strory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            player spelfigur1 = new player();
+            {
+                spelfigur1.name = "kevin";
+                spelfigur1.health = 20;
+            };
+
+
+            Console.WriteLine($"du heter ");
+            Console.WriteLine("Du vaknar upp i en kall fängelsecell.");
+            Console.WriteLine("Det finns en dörr och ett litet fönster");
+            Console.WriteLine("Vad vill du göra");
+            Console.WriteLine("1) öppna dörren \n2) öppna fönstert \n3) leta runt i rummet");
+            
+            string input = Console.ReadLine();
+            if (input == "1")
+            {
+                Console.WriteLine("Det är låst");
+            }
+            else if(input == "2")
+            {
+                Console.WriteLine("Det är låst");
+            }
+            else if (input == "3")
+            {
+                Console.WriteLine("Du hittade en nyckel");
+            }
+
+
+            Console.ReadKey();
+        }
+
+        class player
+        {
+            public string name;
+            public int health;
+        }
+    }
+}
